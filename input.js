96f215cfdb612c90b680ab023182145adaf01cf0
wonderLuc <workmaildo@yandex.ru>
2021-05-09 00:33:21 +0500

    feat: add arguments input

---
+++ input.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const fs = require('fs');
+
+let args = process.argv.slice(2);
+let config = {
+  action: undefined,
+  shift: undefined,
+  inputFile: undefined,
+  outputFile: undefined
+}
+function parseArgs() {
+  args.forEach((arg, i) => {
+    if (i%2 != 0) {
+      return ;
+    }
+    let name = arg;
+    let value = args[i+1];
+    if (name === '-a' || name === '--action') {
+      config.action = value;
+      return ;
+    }
+    if (name === '-s' || name === '--shift') {
+      config.shift = +value;
+      return ;
+    }
+    if (name === '-i' || name === '--input') {
+      let filePath;
+      try{
+        filePath= path.resolve(__dirname, value);
+      }catch(err){
+        missingArgErr();
+      }
+      fs.access(filePath, (err) => {
+        if (err) {
+          process.stderr.write('No such file. Check a path');
+          process.exit(404);
+        }
+        config.inputFile = filePath;
+      }) 
+      return ;
+    }
+    if (name === '-o' || name === '--output') {
+      let filePath;
+      try{
+        filePath= path.resolve(__dirname, value);
+      }catch(err){
+        missingArgErr();
+      }    
+      fs.access(filePath, (err) => {
+        if (err) {
+          process.stderr.write('No such file. Check a path');
+          process.exit(404);
+        }
+        config.outputFile = filePath;
+      })
+      return ;
+    }
+  });
+}
+
+function missingArgErr () {
+  process.stderr.write('Missing Args');
+  process.exit(400);
+}
+async function checkArgs () {
+  await parseArgs();
+  if (!(config.action && config.shift)){
+    missingArgErr();
+  }  
+}
+checkArgs();
+
+module.exports = config;

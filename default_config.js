a6d8fa5bd2e7c361077158cee764f1735a225b05
wonderLuc <workmaildo@yandex.ru>
2021-05-09 21:21:39 +0500

    refactor: remove action property from default settings

--- default_config.js
+++ default_config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 let config = {
-    action: undefined,
     shift: 5,
     inputFile: path.resolve(__dirname,'./input.txt'),
     outputFile: path.resolve(__dirname, './output.txt')

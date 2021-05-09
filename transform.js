1a994cd6635ae1e3d1fdf41d632604e6452d83e6
wonderLuc <workmaildo@yandex.ru>
2021-05-09 18:21:16 +0500

    feat: add transform stream

---
+++ transform.js
@@ -0,0 +1,12 @@
+const {Transform} = require('stream');
+let cipher = require('./cipher_machine.js');
+const config = require('./config_from_Args');
+
+let transformStream = new Transform({transform: (data, encoding, callback) => {
+    data = cipher(data.toString(), config.action);
+    console.log(data);
+    callback(null, data);
+  }
+});
+
+module.exports = transformStream;

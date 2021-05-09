9f4b2defb6c859e48ef2965ebfadae739c899495
wonderLuc <workmaildo@yandex.ru>
2021-05-09 19:53:12 +0500

    refactor: remove console.log

--- transform.js
+++ transform.js
@@ -4,7 +4,6 @@ const config = require('./config_from_Args');
 
 let transformStream = new Transform({transform: (data, encoding, callback) => {
     data = cipher(data.toString(), config.action);
-    console.log(data);
     callback(null, data);
   }
 });
